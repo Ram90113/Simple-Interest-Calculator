7382c60ef8a73368ba5e7e5dcd99521a1d1656ed
Ram90113 <2420090113@klh.edu.in>
2025-09-09 08:33:59 +0530

    simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Simple Interest Calculator
+
+echo "Enter the Principal amount:"
+read principal
+
+echo "Enter the Rate of interest:"
+read rate
+
+echo "Enter the Time (in years):"
+read time
+
+# Calculate Simple Interest
+si=$(( (principal * rate * time) / 100 ))
+
+echo "The Simple Interest is: $si"
